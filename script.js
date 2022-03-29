c11c7a2a9320858e1244deb7860a0b9559e666a0
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-29 15:45:28 -0400

    Finished JavaScript File

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const info = function(){
+// Function Expression
+const calculate = function () {
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3; 
-const team1Scores = calcAverage(44, 530, 71);
-const team2Scores = calcAverage(65, 54, 49);
+    // Arrow Function
+    const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-const checkWinner = team1Scores > team2Scores ? `Team 1 wins with a score of ${team1Scores} vs ${team2Scores}` : `Team 2 wins with a score of ${team2Scores} vs ${team1Scores}.`;
-document.querySelector("h3").textContent = checkWinner;
+    // calcAverage called twice with two different set values and stored in two different variables
+    const team1Scores = calcAverage(44, 23, 71);
+    const team2Scores = calcAverage(65, 540, 49);
 
+    // The winning team must score at least twice as much as the other team to win
+    if (team1Scores >= 2 * team2Scores) {
+        document.querySelector("h3").textContent = `Team 1 wins with a score of ${team1Scores} vs ${team2Scores}.`;
+    }
+    else if (team2Scores >= 2 * team1Scores) {
+        document.querySelector("h3").textContent = `Team 2 wins with a score of ${team2Scores} vs ${team1Scores}.`;
+    }
+    else {
+        document.querySelector("h3").textContent = `Neither team quailifies.`;
+    }
 }
