9abdb8d3125838ba46f4b5909dda8bd3380df946
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-28 21:22:08 -0400

    added an arrow function and Ternary Operator

---
+++ script.js
@@ -0,0 +1,14 @@
+'strict'
+
+const info = function(){
+
+const calcAverage = () => (score1 + score2 +score3) / 3; 
+const avgTeam1 = (44, 23, 71);
+const avgTeam2 = (65, 54, 49);
+console.log(avgTeam1 + ' team1');
+console.log(avgTeam2 + ' team2');
+
+
+const checkWinner = avgTeam1 > avgTeam2 ? 'team 1 wins' : 'team 2 wins';
+console.log(checkWinner);
+}
